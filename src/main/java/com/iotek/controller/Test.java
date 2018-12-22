fc30f7206d9a976a4c9a88fbe340d7947f6fe412
0ZQXRL5HF2CAHVI\Administrator <y13205117@gmail.com>
2018-12-23 02:00:08 +0800

    初始化配置

--- src/main/java/com/iotek/controller/Test.java
+++
@@ -1,7 +0,0 @@
-package com.iotek.controller;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("hi");
-    }
-}
--- src/main/java/com/iotek/model/User.java
+++ src/main/java/com/iotek/model/User.java
@@ -1,7 +1,42 @@
 package com.iotek.model;
 
 import java.io.Serializable;
-
+//游客
 public class User implements Serializable {
     private Integer id;
+    private String uname;
+    private String upass;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getUpass() {
+        return upass;
+    }
+
+    public void setUpass(String upass) {
+        this.upass = upass;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", uname='" + uname + '\'' +
+                ", upass='" + upass + '\'' +
+                '}';
+    }
 }
